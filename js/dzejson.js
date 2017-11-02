750f72bdf77e48a1543b3966ca71a15021b4eb61
Lukasz-Samsung <l.sitnik@samsung.com>
2017-11-02 16:38:00 +0100

    menuBYjson: Stworzenie cześci jsona oraz pierwsze testowe próby oprogramowania

---
+++ js/dzejson.js
@@ -0,0 +1,188 @@
+
+
+var data = {
+	"menu"  : [
+	{
+		"name" : "Strona Główna",
+		"url" : "/",
+		"extand" : false
+	},
+	{
+		"name" : "Bogowie",
+		"url" : "/podstrony/bogowie.html",
+		"extand" : true,
+        "extand-content" : [
+        {
+            
+            "name" : "An",
+            "url" : "/podstrony/podstrony_poziom_1_bogowie/an.html",
+            "extand" : false
+        },
+        {
+            "name" : "Enlil",
+            "url" : "/podstrony/podstrony_poziom_1_bogowie/enlil.html",
+            "extand" : false
+        },
+        {
+            "name" : "Enki",
+            "url" : "/podstrony/podstrony_poziom_1_bogowie/enki.html",
+            "extand" : false
+        },
+        {
+            "name" : "Nanna",
+            "url" : "/podstrony/podstrony_poziom_1_bogowie/nanna.html",
+            "extand" : false
+        },
+        {
+            "name" : "Utu",
+            "url" : "/podstrony/podstrony_poziom_1_bogowie/utu.html",
+            "extand" : false
+        },
+        {
+            "name" : "Inanna",
+            "url" : "/podstrony/podstrony_poziom_1_bogowie/inanna.html",
+            "extand" : false
+        },
+        {
+            "name" : "Dumuzi",
+            "url" : "/podstrony/podstrony_poziom_1_bogowie/dumuzi.html",
+            "extand" : false
+        },
+        {
+            "name" : "Ereszkigal",
+            "url" : "/podstrony/podstrony_poziom_1_bogowie/ereszkigal.html",
+            "extand" : false
+        },
+        {
+            "name" : "Iszkur",
+            "url" : "/podstrony/podstrony_poziom_1_bogowie/iszkur.html",
+            "extand" : false
+        },
+        {
+            "name" : "Gesztinanna",
+            "url" : "/podstrony/podstrony_poziom_1_bogowie/gesztinanna.html",
+            "extand" : false
+        },
+        {
+            "name" : "Ningisszida",
+            "url" : "/podstrony/podstrony_poziom_1_bogowie/ningisszida.html",
+            "extand" : false
+        }
+            ]
+    },
+	{
+		"name" : "Piśmiennictwo",
+		"url" : "/podstrony/mitologia.html",
+		"extand" : true,
+        "extand-content" : [
+            
+            {
+                "name" : "Sumeryjska lista królów",
+                "url" : "/podstrony/podstrony_poziom_1_podania/sumeryjska_lista_krolow.html",
+                "extand" : false
+            },
+            {
+                "name" : "Sumeryjska lista królów",
+                "url" : "/podstrony/podstrony_poziom_1_podania/sumeryjska_lista_krolow.html",
+                "extand" : false
+            },
+            {
+                "name" : "Potop",
+                "url" : "/podstrony/podstrony_poziom_1_podania/potop.html",
+                "extand" : false
+            },
+            {
+                "name" : "Inskrypcja z Tummal",
+                "url" : "/podstrony/podstrony_poziom_1_podania/inskrypcja_tummal.html",
+                "extand" : false
+            },
+            {
+                "name" : "Gilgamesz i Agga",
+                "url" : "/podstrony/podstrony_poziom_1_podania/gilgamesz_agga.html",
+                "extand" : false
+            },
+            {
+                "name" : "Gilgamesz i Agga",
+                "url" : "/podstrony/podstrony_poziom_1_podania/gilgamesz_agga.html",
+                "extand" : false
+            },
+            {
+                "name" : "Gilgamesz",
+                "url" : "/podstrony/podstrony_poziom_1_podania/gilgamesz.html",
+                "extand" : false
+            },
+            {
+                "name" : "Enuma Elisz",
+                "url" : "/podstrony/podstrony_poziom_1_podania/enuma_elisz.html",
+                "extand" : false
+            }
+        ]
+	},
+	{
+		"name" : "Sztuka",
+		"url" : "/podstrony/sztuka.html",
+		"extand" : true,
+        "extand-content" : [
+            
+            {
+                "name" : "Sztuka zdobnicza",
+                "url" : "/podstrony/podstrony_poziom_1_podania/gliptyka.html",
+                "extand" : true,
+                "extand-content" : {}
+            },
+            {
+                "name" : "Rzeźba",
+                "url" : "/podstrony/podstrony_poziom_1_podania/rzezba.html",
+                "extand" : false
+            },
+            {
+                "name" : "Sztuka zdobnicza",
+                "url" : "/podstrony/podstrony_poziom_1_podania/gliptyka.html",
+                "extand" : true,
+                "extand-content" : {}
+            }
+        ]
+	},
+	{
+		"name" : "Architektura",
+		"url" : "/podstrony/architektura.html",
+		"extand" : true
+	},
+	{
+		"name" : "Miasta",
+		"url" : "/podstrony/miasta.html",
+		"extand" : true
+	},
+	{
+		"name" : "Historia",
+		"url" : "/podstrony/historia.html",
+		"extand" : true
+	},
+	{
+		"name" : "Prawo",
+		"url" : "/podstrony/prawo.html",
+		"extand" : true
+	},
+	{
+		"name" : "Nauka",
+		"url" : "/podstrony/nauka.html",
+		"extand" : true
+	},
+	{
+		"name" : "Pismo",
+		"url" : "/podstrony/pismo.html",
+		"extand" : false
+	},
+	{
+		"name" : "Księga Gości",
+		"url" : "http://net3.pl/uslugi/ksiega.php?p_user=sitnikl1&p_nrksiegi=0",
+		"extand" : false
+	},
+	{	
+		"name" : "Linki",
+		"url" : "/podstrony/linki.html",
+		"extand" : false
+	}
+	]
+
+}
