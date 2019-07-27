8b5ac945bca40c74d1bf108eb699cb360228e7ea
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2019-07-27 23:14:15 +0200

    "Aside Bar" module refactored

--- js/modules/AsideBar.js
+++ js/modules/AsideBar.js
@@ -4,73 +4,76 @@ import news from './News.js';
 
 const asideBar = {
     displaySideBar() {
-        const setWaterMark = (url, alternativeText) => {
-            const imgElement = document.createElement("img");
-            imgElement.setAttribute("class", "bocznyDingir");
-            imgElement.setAttribute("src", url);
-            imgElement.setAttribute("alt", alternativeText);
-            menu.menuIDElement.appendChild(imgElement)
+        this.showAsideBarElements();
+    },
+    buildWaterMark(url, alternativeText) {
+        const imgElement = document.createElement("img");
+        imgElement.setAttribute("class", "bocznyDingir");
+        imgElement.setAttribute("src", url);
+        imgElement.setAttribute("alt", alternativeText);
+        menu.menuIDElement.appendChild(imgElement)
+    },
+    setHeader(text) {
+        const spanElement = document.createElement("span");
+        menu.menuIDElement.appendChild(spanElement);
+        spanElement.innerHTML = text;
+    },
+    getFromVariable(index) {
+        switch (index) {
+            case 2: return 0;
+                break;
+            case 4: return 4;
+                break;
+            case 6: return 8;
+                break;
         }
-
-        const setHeader = text => {
-            const spanElement = document.createElement("span");
-            menu.menuIDElement.appendChild(spanElement);
-            spanElement.innerHTML = text;
+    },
+    getToVariable(index) {
+        switch (index) {
+            case 2: return 3;
+                break;
+            case 4: return 7;
+                break;
+            case 6: return 8;
+                break;
         }
+    },
+    getHeader(index) {
+        switch (index) {
+            case 1: return asideHeaders.header1;
+                break;
+            case 3: return asideHeaders.header2;
+                break;
+            case 5: return asideHeaders.header3;
+                break;
+        }
+    },
+    showAsideBarElements() {
+        const asideDingir = watermark.path;
+        const asideDingirAltText = watermark.alt;
+        const numberOfTiles = 9;
 
-        const showTiles = () => {
-            const asideDingir = watermark.path;
-            const asideDingirAltText = watermark.alt;
-            const numberOfTiles = 8;
-
-            const getFromVariable = index => {
-                switch (index) {
-                    case 2: return 0;
-                        break;
-                    case 4: return 4;
-                        break;
-                    case 6: return 8;
-                        break;
-                }
-            }
-
-            const getToVariable = index => {
-                switch (index) {
-                    case 2: return 3;
-                        break;
-                    case 4: return 7;
-                        break;
-                    case 6: return 8;
-                        break;
-                }
-            }
-
-            const getHeader = index => {
-                switch (index) {
-                    case 1: return asideHeaders.header1;
-                        break;
-                    case 3: return asideHeaders.header2;
-                        break;
-                    case 5: return asideHeaders.header3;
-                        break;
-                }
-            }
-
-            for (let i = 0; i < numberOfTiles; i++) {
-                if (i === 0 || i === 7) {
-                    setWaterMark(asideDingir, asideDingirAltText);
-                } else if (i === 2 || i === 4 || i === 6) {
-                    let from = getFromVariable(i);
-                    let to = getToVariable(i);
-                    news.displayBlock(from, to);
-                } else {
-                    let header = getHeader(i);
-                    setHeader(header);
-                }
+        for (let i = 0; i < numberOfTiles - 1; i++) {
+            if (i === 0 || i === 7) {
+                this.addWatermark(asideDingir, asideDingirAltText);
+            } else if (i === 2 || i === 4 || i === 6) {
+                this.addTilesSeries(i);
+            } else {
+                this.addHeader(i);
             }
         }
-
-        showTiles();
+    },
+    addWatermark(asideDingir, asideDingirAltText) {
+        this.buildWaterMark(asideDingir, asideDingirAltText);
+    }, 
+    addTilesSeries(i) {
+        let from = this.getFromVariable(i);
+        let to = this.getToVariable(i);
+        news.displayBlock(from, to);
+    },
+    addHeader(i) {
+        let header = this.getHeader(i);
+        this.setHeader(header);
     }
 };
 
