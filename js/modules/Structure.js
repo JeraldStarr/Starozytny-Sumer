f27a10e1c711a5b6df7afd9a181b2f82bf8b9d7b
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2019-10-22 00:03:31 +0200

    Refactored 'structure' module code

--- js/modules/Structure.js
+++ js/modules/Structure.js
@@ -2,19 +2,31 @@ const structure = {
     insertSectionToHeader() {
         const header = document.getElementById("NAGLOWEK");
         const section = document.createElement("section");
+        const spanHTML = this.buildSpan();
+        const waterMarkContainers = this.buildWaterMarkContainers();  
+        header.appendChild(section);
+        section.appendChild(waterMarkContainers[0]);
+        section.appendChild(spanHTML);
+        section.appendChild(waterMarkContainers[1]);
+        
+    },
+    buildSpan() {
         const span = document.createElement("span");
+        span.classList.add("sectionTitle");
+        span.innerText = document.querySelector("h1").innerText;
+        return span;
+    },
+    buildWaterMarkContainers() {
+        const waterMarkContainers = [];
         const beforeWaterMarkContainer = document.createElement("div");
         const afterWaterMarkContainer = document.createElement("div");
         beforeWaterMarkContainer.classList.add("waterMarkTitle");
         afterWaterMarkContainer.classList.add("waterMarkTitle");
-        span.classList.add("sectionTitle");
-        header.appendChild(section);
-        section.appendChild(beforeWaterMarkContainer);
-        section.appendChild(span);
-        section.appendChild(afterWaterMarkContainer);
-        span.innerText = document.querySelector("h1").innerText;
-
+        waterMarkContainers.push(beforeWaterMarkContainer, afterWaterMarkContainer);
+        // waterMarkContainers.push(afterWaterMarkContainer);
+        return waterMarkContainers;
     }
+
 }
 
 export default structure;
