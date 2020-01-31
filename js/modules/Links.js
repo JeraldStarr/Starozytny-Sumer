489eef55e0310541937267c98242e45d69297e53
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2020-02-01 00:51:43 +0100

    Fixed addiing tag "#lokalizacja" to appropriate links

--- js/modules/Links.js
+++ js/modules/Links.js
@@ -9,20 +9,31 @@ const links = {
     addTagToLinks() {
         this.addTagToLinksInSection("TRESC");
         this.addTagToLinksInSection("lokalizacja");
+        if (document.getElementById("content")) {
+            this.addTagToLinksInSection("content");
+        }
     },
     addTagToLinksInSection(selector) {
         const section = document.getElementById(selector);
         const linksCollection = section.getElementsByTagName("a");
         const validLinksCollection = [];
-        for (var i = 0; i < linksCollection.length; i++) {  
-            this.addTagToLink(linksCollection, i, validLinksCollection);
+        console.log();
+        for (var i = 0; i < linksCollection.length; i++) { 
+            if (selector === "TRESC") {
+                if (linksCollection[i].title
+                    .includes("Proszę kliknąć, aby przejść do artykułu:")) {
+                    this.addTagToLink(linksCollection, i, validLinksCollection);
+                };
+            } else if (selector === "lokalizacja" || selector === "content") {
+                this.addTagToLink(linksCollection, i, validLinksCollection);
+            }
         };
     },
     addTagToLink(linksCollection, i, validLinksCollection) {     
         linksCollection[i].setAttribute("href", `${linksCollection[i]
             .href}#lokalizacja`);
         validLinksCollection.push(linksCollection[i]);    
-    }
+    },
 }
 
 export default links;
