bf744b6f4c082afe4f4fb7edae861e1d4c450d17
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2020-02-15 13:56:34 +0100

    Fixed script adding hash tags to links in sitemap

--- js/modules/Links.js
+++ js/modules/Links.js
@@ -9,33 +9,19 @@ const links = {
     addTagToLinks() {
         this.addTagToLinksInSection("TRESC");
         this.addTagToLinksInSection("lokalizacja");
-<<<<<<< HEAD
-        if (document.getElementById("content")) {
-            this.addTagToLinksInSection("content");
-        }
-=======
-        this.addTagToLinksInSection("contain");
->>>>>>> c9e7338f932a38073b2a5b6fc78d6753189cf5a1
+        this.addTagToLinksInSection("content");
     },
     addTagToLinksInSection(selector) {
         const section = document.getElementById(selector);
         const linksCollection = section.getElementsByTagName("a");
         const validLinksCollection = [];
-<<<<<<< HEAD
-        console.log();
-=======
->>>>>>> c9e7338f932a38073b2a5b6fc78d6753189cf5a1
         for (var i = 0; i < linksCollection.length; i++) { 
             if (selector === "TRESC") {
                 if (linksCollection[i].title
                     .includes("Proszę kliknąć, aby przejść do artykułu:")) {
                     this.addTagToLink(linksCollection, i, validLinksCollection);
                 };
-<<<<<<< HEAD
             } else if (selector === "lokalizacja" || selector === "content") {
-=======
-            } else if (selector === "lokalizacja" || selector === "contain") {
->>>>>>> c9e7338f932a38073b2a5b6fc78d6753189cf5a1
                 this.addTagToLink(linksCollection, i, validLinksCollection);
             }
         };
