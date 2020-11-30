db6d1de27a007d9e59d1c650c6918a21c8fe7863
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2020-11-30 23:32:07 +0100

    Modified message view after sending a message form contact form

--- php/mail.php
+++ php/mail.php
@@ -9,197 +9,23 @@ $headers = 'From:' . $_POST['email'] . "\r\n" .
 <html lang="pl">
 <head>
 	<meta charset="utf-8" />
-
-	
-	
-	     <meta name="Generator" content="kED 2.1.4.0">
-		 <meta name="description" content="Strona kontaktowa www.starozytnysumer.pl" />
-         <meta name="author" content="Łukasz Sitnik">
-         <meta name="Keywords" content="kontakt, autor" />
-         <meta name="reply-To" content="lukasz.sitnik@gmail.com" />
-         <meta name="creation-date" content="2010-09-30">
-	     <meta name="last-modified" content="2016-04-14"/>
-	     <link rel="Shortcut icon" href="/starozytny_sumer.ico" />
-
-	
-       
-    <title>Kontakt</title>
+	<title>Kontakt</title>
 	<style>
-	label {
-		font-weight: bold;
-		font-family: "Times New Roman";
-		color: black;
-	}
-	
-	#form {
-		border: 1px solid black;
-		margin: 0 auto;
-		padding: 10%;
-		width: 60%;
-		margin-top: 35px;
-		background-color: #FCFCE1;
-		border-radius: 4px;
-	}
-	
-	input[type="text"]#e-mail {
-		margin-left:3%;
-	}	
-	input[type="text"]#subject {
-		margin-left:11.3%;
-	}
-	input[type="submit"], input[type="reset"] {
-		background: #702525;
-		color: white;
-		border-radius: 4px;
-		border: 3px solid lightgray;
-	}
-
-	input[type="submit"]:hover, input[type="reset"]:hover {
-		opacity: .8;
-	}
-
-	input[type="submit"]:active, input[type="reset"]:active {
-		color: red;
-	}
-
-	#email {
-		font-size: 12px;
-		margin-top: 80px;
-		}
-	
-	input.invalid {
-		background-color: #FF9;
-		border: 2px red inset;
-		}
-	
-	textarea.invalid {
-		background-color: #FF9;
-		border: 2px red inset;
-		}
-
-	label.invalid {
-		color: #F00;
-		font-weight: bold;
-		}
-
-	form div#wrong {
-		margin: 0 0 0 25%;
-		color: black;
-		font-size: 14px;
-		font-weight: 300;
-		color: red;
-		font-size: 13px;
-	}
-	</style>
-	
-
+	.contactMessage{background:linear-gradient(150deg, white, #f7f7e7 1000px);width:100vw;height:100vh}.contactMessage__text{font-size:50px;text-align:center;position:relative;top:30vh}.contactMessage__button{position:relative;left:50%;top:60%;-webkit-transform:translate(-50%, -50%);transform:translate(-50%, -50%);background:#702525;color:white;border-radius:4px;border:3px solid lightgray;-webkit-transition:.3s;transition:.3s;width:100px;height:40px;font-size:20px;cursor:pointer}.contactMessage__button:hover{opacity:.8}
+/*# sourceMappingURL=contactMessage.css.map */
+</style>
 </head>
 
-  <body>
-	<div id="calosc">
-	<div id="NAGLOWEK">
-
-	<a href="/"><img id="goraStrony" src="/grafika/zdjecia/zikkurat_2.JPG" alt="Wierzchołek zikkuratu"/></a>
-
-	</div>
-	
-	<div class="szukaj">
-
-	Szukaj na stronie...
-	</div>
-
-	<div class="wyszukiwarka">
-
-		<script src="/js/wyszukiwarka.js"></script>
-	
-	</div>
-
-	<div id="lokalizacja">
-	<p class="sitemap"><a href="/sitemap.html" title="Proszę kliknąć, aby wyświetlić mapę serwisu">mapa strony</a></p>
-	tutaj jesteś: <a href="/" title="Proszę kliknąć, aby przejść do strony głównej">Strona Główna</a> >> Kontakt
-	</div>
-	
-
-	<div id="TRESC">
-
-		<h1>Kontakt</h1><hr class="podkreslenie"/>
-
-
-	
-	<form action="/php/mail.php" method="post" id="form">
-	
-	<div style="margin: 0 25%;">Wiadomość została wysłana</div>
-	
-	</form>
-	<p id="email">
-e-mail: <strong>lukasz.sitnik@gmail.com</strong>
-	</p>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-
-	<div class="gora_strony"><a href="#goraStrony">góra strony</a></div>
-	<img class="dingir" src="/grafika/zdjecia/dingir/dingir_przezroczysty.gif" alt="Skrzydlaty dysk - symbol bogów sumeryjskich"/>
-   <br/>
-   <br/>
-
-
-	</div>
-	
-
-
-	<div id="MENU">
-
-	
-
-
-
-	</div>
-
-
-
-	<div id="STOPKA">
-
-	<br/>
-
-	<p class="linki_dol_strony">
-<a href="/inne_podstrony/kontakt.html" title="Proszę kliknąć, aby wyświetlić dane kontaktowe autora strony">kontakt</a><br/>
-<a href="/inne_podstrony/statystyka.html">statystyka wejść</a>
-   </p>
-
-
-	</div>
-	</div>
-	
-	<!-- CSS -->
-	<link rel="stylesheet" href="/style.css" type="text/css" />
-	<!-- CSS - wtyczka fancy box -->
-	<link href="/fancybox/jquery.fancybox-1.3.4.css" rel="stylesheet">
-	<!-- jquery libery-->
-	<script src="/js/jquery-1.6.3.min.js"></script>
-	<!--  easing plugin -->
-	<script src="/js/jquery.easing.1.3.js"></script>
-	<!--  fancybox plugin -->
-	<script src="/fancybox/jquery.fancybox-1.3.4.min.js"></script>
-	<!-- the file stores a data -->
-	<script src="/js/dataStore.js"></script>  
-	<!-- module design pattern -->
-	<script src="/js/javaScriptStore.js"></script>
-	<!-- main js -->
-	<script src="/js/add.js"></script>
-	<!-- users tracking by google analitics -->
-	<script src="/js/tracking.js"></script>
-
+<body class="contactMessage">
+	<div class="contactMessage__text">Wiadomość została wysłana</div>
+	<button class="contactMessage__button">OK</button>
+	<script>
+		document.querySelector(".contactMessage__button").addEventListener("click", () => {
+			window.location.href = "https://www.starozytnysumer.pl/inne_podstrony/kontakt.html#lokalizacja";
+	});
+</script>
 </body>
 
 </html>
-
-
-
 ';
 ?>
